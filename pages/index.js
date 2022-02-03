ddf2a6e4eb45dfbce9c4a3b1c4c01c1902e28c0e
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-02-04 02:10:18 +0530

    Basic fuzzy search finished ✅ with frontend
    TODO: Make the search hit list pretty and add images of the hit cars

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import CarForm from "../lib/CarForm";
-
+import SearchForm from "../lib/SearchForm";
 export default function Home() {
   return (
     <>
+      <h1 className="mt-5 text-3xl text-center">Search for a Car</h1>
+      <SearchForm />
       <h1 className="mt-5 text-3xl text-center">Create a Car</h1>
       <CarForm />
     </>
